3d2578368ccd97f3bd9f9a6b272fc259e58414a0
Daigneault Pearce <Daigneault.PardoPearce.515@my.csun.edu>
2018-04-26 23:39:02 -0700

    3 string version

--- Project2.java
+++ Project2.java
@@ -17,8 +17,10 @@ public class Project2 {
 	static String x = null;
 	//String 2
 	static String y = null;
+	//String 3
+	static String z = null;
 	//Dynamic table
-	static int[][] dTable;
+	static int[][][] dTable;
 	
 	public static void main(String[] args) throws IOException{
 		
@@ -26,13 +28,10 @@ public class Project2 {
 		readIn();
 		//Initialize table
 		tableInit();
-		//Print table
-		tablePrint();
 		//Populate table
-		System.out.println("\n" + tablePopulate());
-		
-		
-		
+		tablePopulate();
+		//Output value of last cell which contains the answer
+		System.out.println(dTable[y.length()][x.length()][z.length()]);		
 	}
 	
 	public static void readIn() throws IOException{
@@ -40,58 +39,49 @@ public class Project2 {
 		Scanner scan = new Scanner ( new File ( "input2.txt" ) );
 		y = scan.next();
 		x = scan.next();
-		scan.close();
-		
+		z = scan.next();
+		scan.close();	
 	}
 	
 	public static void tableInit() {
 		
 		//create dynamic programming table based of input value lengths.
-		dTable = new int[ y.length() + 1 ][ x.length() + 1 ];
+		dTable = new int[ y.length() + 1 ][ x.length() + 1 ][ z.length() + 1 ];
 		
 		//initialize zeros.
 		for( int i = 0; i <= y.length(); i++ )
-			dTable[i][0] = 0;
+			dTable[i][0][0] = 0;
 		
 		for( int i = 0; i <= x.length(); i++)
-			dTable[0][i] = 0;
+			dTable[0][i][0] = 0;
+		
+		for( int i = 0; i <= z.length(); i++)
+			dTable[0][0][i] = 0;
 		
 	}
 	
-	public static void tablePrint() {
+	public static void tablePopulate() {
 		
-		for(int i = 0; i <= y.length(); i++) {
-			System.out.println();
-			for(int j = 0; j <= x.length(); j++)
-				System.out.print(dTable[i][j]);
-			
-		}
-	}
-	
-	public static int tablePopulate() {
-		
-		//initialize variables to hold character values
+		//Initialize variables to hold character values
 		String x_sub;
 		String y_sub;
-		int max = 0;
+		String z_sub;
 		
+		//Assign a value to the cell.
 		for( int i = 1; i <= y.length(); i++ ) {
 			y_sub = y.substring( i - 1, i );
-			//System.out.print(y_sub);
 			for( int j = 1; j <= x.length(); j++ ) {
 				x_sub = x.substring( j - 1, j );
-				//System.out.print(x_sub);
-				if( x_sub.equals(y_sub) ) {
-					dTable[i][j] = 1 + dTable[i - 1][j - 1];
+				for (int k = 1; k <= z.length(); k++) {
+					z_sub = z.substring( k - 1, k );
+					//If all characters are equal increment by 1
+					if( x_sub.equals(y_sub) && y_sub.equals(z_sub) )
+						dTable[i][j][k] = 1 + dTable[ i - 1 ][ j - 1 ][ k - 1 ];
+					//Otherwise assign largest adjacent cell
+					else
+						dTable[i][j][k] = Math.max(Math.max(dTable[ i - 1 ][ j ][ k ], dTable[ i ][ j ][ k - 1 ]), dTable[ i ][ j - 1 ][ k ]);
 				}
-				else {
-					dTable[i][j] = Math.max(dTable[ i - 1 ][ j ],dTable[ i  ][ j - 1 ]);
-				}
-				//update max
-				max = dTable[i][j];
 			}
 		}
-		tablePrint();
-		return max;
 	}
 }
